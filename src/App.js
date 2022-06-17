846727e66ffc165bef3e5d5a1ddce23d1bf00c4d
Matheus Moraes de Faria <36715779+MatheusMoraes66@users.noreply.github.com>
2022-06-16 21:31:04 -0300

    TaskCreator -> component para o formulario de Task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
+import { TaskCreator } from "./components/TaskCreator";
+import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+     <TaskCreator/>
     </div>
   );
 }
