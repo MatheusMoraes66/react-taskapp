e7ea1fb77708891c88f202393deea535863e078f
Matheus Moraes de Faria <36715779+MatheusMoraes66@users.noreply.github.com>
2022-06-16 22:06:45 -0300

    App -> criação de uma função que salva as tarefas no localstorage

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
 import { TaskCreator } from "./components/TaskCreator";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-const [tasksItems, setTasksItems] = useState([
-  { name: "Minha primeira tarefa", done: false},
-  { name: "Minha segunda tarefa", done: false},
-  { name: "Minha terceira tarefa", done: false},
-])
+  const [tasksItems, setTasksItems] = useState([]);
 
-const createNewTask = (taskname) => {
-  const isExistTask = tasksItems.find(task => task.name === taskname)
-  if(!isExistTask){
-    setTasksItems([...tasksItems, {name: taskname, done: false}])
-  }
-}
+  useEffect(() => {
+    let data = localStorage.getItem("tasks");
+    if (data) {
+      setTasksItems(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasksItems));
+  }, [tasksItems]);
+
+  const createNewTask = (taskname) => {
+    const isExistTask = tasksItems.find((task) => task.name === taskname);
+    if (!isExistTask) {
+      setTasksItems([...tasksItems, { name: taskname, done: false }]);
+    }
+  };
+
   return (
     <div className="App">
-     <TaskCreator createNewTask={createNewTask}/>
-     <table>
-       <thead>
-         <tr>
-           <th>Tasks</th>
-         </tr>
-       </thead>
-       <tbody>
-       {
-       tasksItems.map((task, index) => {
-         return (
-           <tr key={index}>
-             <td>
-              {task.name}
-             </td>
-           </tr>
-         )
-       })
-     }
-       </tbody>
-     </table>
-     
+      <TaskCreator createNewTask={createNewTask} />
+      <table>
+        <thead>
+          <tr>
+            <th>Tasks</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasksItems.map((task, index) => {
+            return (
+              <tr key={index}>
+                <td>{task.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
