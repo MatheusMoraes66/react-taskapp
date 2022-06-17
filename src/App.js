b0bd0d5525a5ff3b95999c4cf693e2932f5eecd3
Matheus Moraes de Faria <36715779+MatheusMoraes66@users.noreply.github.com>
2022-06-16 21:54:01 -0300

    App -> criação da lista de task e função para criar novas tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import { TaskCreator } from "./components/TaskCreator";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+const [tasksItems, setTasksItems] = useState([
+  { name: "Minha primeira tarefa", done: false},
+  { name: "Minha segunda tarefa", done: false},
+  { name: "Minha terceira tarefa", done: false},
+])
 
+const createNewTask = (taskname) => {
+  const isExistTask = tasksItems.find(task => task.name === taskname)
+  if(!isExistTask){
+    setTasksItems([...tasksItems, {name: taskname, done: false}])
+  }
+}
   return (
     <div className="App">
-     <TaskCreator/>
+     <TaskCreator createNewTask={createNewTask}/>
+     <table>
+       <thead>
+         <tr>
+           <th>Tasks</th>
+         </tr>
+       </thead>
+       <tbody>
+       {
+       tasksItems.map((task, index) => {
+         return (
+           <tr key={index}>
+             <td>
+              {task.name}
+             </td>
+           </tr>
+         )
+       })
+     }
+       </tbody>
+     </table>
+     
     </div>
   );
 }
